c06aa39af112f5b3c10cd6ad1921db546db0f753
Sebahattin ÖZDEMİR <sebahattinozdmr@gmail.com>
2022-04-17 21:52:43 +0300

    deply

---
+++ build/precache-manifest.d432d2e2bffe7a82c03b4f0d1dee5e66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "284df61c138cf738ba250b79a7ef153d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7682b8e4ad364290b67a",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7682b8e4ad364290b67a",
+    "url": "/static/js/main.8defb031.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);

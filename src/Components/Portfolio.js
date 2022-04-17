5b0bbe1f03aac8411f7f55d8ca4223569e9de8e0
Sebahattin ÖZDEMİR <sebahattinozdmr@gmail.com>
2022-04-17 23:01:53 +0300

    reverted

---
+++ src/Components/Portfolio.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+
+class Portfolio extends Component {
+  render() {
+    if (this.props.data) {
+      var projects = this.props.data.projects.map(function (projects) {
+        var projectImage = "images/portfolio/" + projects.image;
+        return (
+          <div key={projects.title} className="columns portfolio-item">
+            <div className="item-wrap">
+              <a href={projects.url} title={projects.title}>
+                <img alt={projects.title} src={projectImage} />
+                <div className="overlay">
+                  <div className="portfolio-item-meta">
+                    <h5>{projects.title}</h5>
+                    <p>{projects.category}</p>
+                  </div>
+                </div>
+                <div className="link-icon">
+                  <i className="fa fa-link"></i>
+                </div>
+              </a>
+            </div>
+            {projects.title}
+          </div>
+        );
+      });
+    }
+
+    return (
+      <section id="portfolio">
+        <div className="row">
+          <div className="twelve columns collapsed">
+            <h1>Check Out My Personal Projects.</h1>
+
+            <div
+              id="portfolio-wrapper"
+              className="bgrid-quarters s-bgrid-thirds cf"
+            >
+              {projects}
+            </div>
+            <b style={{ padding: "20px" }}>
+              You can find source code from my github account.
+            </b>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Portfolio;

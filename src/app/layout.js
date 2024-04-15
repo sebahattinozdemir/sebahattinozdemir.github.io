6f23fae2f9831781843e023ed6e3cfab20444799
Sebahattin <sebahattinozdmr@gmail.com>
2024-04-15 17:09:17 +0300

    new interface

---
+++ src/app/layout.js
@@ -0,0 +1,20 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import TransitionProvider from "@/components/transitionProvider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Sebahattin Ozdemir | Frontend Developer",
+  description: "Senior Frontend Developer Hire",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <TransitionProvider>{children}</TransitionProvider>
+      </body>
+    </html>
+  );
+}
